5a52dce6801ffe85ef428bdd90575e81266723e5
magicLost <oooze@mail.ru>
2022-04-26 20:09:13 +0300

    #Fix bug - 432

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * as cloudinary from "./service/cloudinary";
 export * as googleDrive from "./service/googleDrive";
 export * as firestore from "./service/firestore";
-
-export { default as validateMulterReqParams } from "./middleware/validateReqParams/multerValidationMiddleware";
-
-export { default as validateReqParams } from "./middleware/validateReqParams/validationMiddleware";
+export * as firebaseAuth from "./service/firebase-auth";
--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,3 @@
-////////////////////
-/* 
-export type OriginalPhotoInfo = {
-  id?: string | null;
-  name?: string | null;
-}; */
-
-/* export type PhotoInfo = {
-  aspectRatio: number;
-  imageExtention: string;
-  isInverted: boolean;
-  width: number;
-  height: number;
-  size?: number;
-};
-
-export type WebImageInfo = {
-  id: PhotoWebId;
-  url: WebSecureUrl;
-}; */
-
-/* export type WebImagesInfo = {
-    ids: PhotoWebId[];
-    urls: Map<Width, WebSecureUrl>;
-  }; */
-/* 
-export type TransformedImageInfo = {
-  format: string;
-  size: number;
-  width: number;
-  height: number;
-  buffer?: Buffer;
-}; */
-
 export type Path = string;
 
 export type Width = number;
